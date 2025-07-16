1ede73f14cd1f54a3cb683f69961743184d944eb
Nico Th. Stolz <nico@everstorm.de>
2025-07-16 10:43:49 +0200

    fix: More slots

--- src/Gear.js
+++ src/Gear.js
@@ -1,65 +1,72 @@
-import './Gear.css';
-import { useState } from 'react';
+import "./Gear.css";
+import { useState } from "react";
 
 function Gear() {
+    const [additionalSlots, setAdditionalSlots] = useState([]);
 
-	let additionalSlots = [];
-	const listItems = 
-		<div key={0}>
-			<InventoryItem itemname={""} amount={0}></InventoryItem>
-			{additionalSlots}
-			<p><button onClick={onClick}>add</button> more slots</p>
-		</div>
+    const listItems = (
+        <div>
+            <InventoryItem itemname={""} amount={0}></InventoryItem>
+            {additionalSlots}
+            <p>
+                <button onClick={onClick}>add</button> more slots
+            </p>
+        </div>
+    );
 
-	function onClick() {
-		additionalSlots.push(<InventoryItem key={additionalSlots.length} itemname={""} amount={0}></InventoryItem>);
-	}
+    function onClick() {
+        setAdditionalSlots((prevSlots) => [
+            ...prevSlots,
+            <InventoryItem key={'ii' + prevSlots.length} itemname={""} amount={0}></InventoryItem>,
+        ]);
+    }
 
-	return (
-		<div className='Gear'>
-			<h3>Gear</h3>
-			{listItems}
-		</div>
-	);
+    return (
+        <div className="Gear">
+            <h3>Gear</h3>
+            {listItems}
+        </div>
+    );
 }
 
 function InventoryItem({ itemname, amount }) {
-	const [_amount, setAmount] = useState(amount);
-	const [_name, setName] = useState(itemname);
+    const [_amount, setAmount] = useState(amount);
+    const [_name, setName] = useState(itemname);
 
-	function handleClickUse() {
-		if (_amount <= 1) {
-			setName("");
-		}
-		setAmount(_amount - 1);
-	}
+    function handleClickUse() {
+        if (_amount <= 1) {
+            setName("");
+        }
+        setAmount(_amount - 1);
+    }
 
-	function handleClickAdd() {
-		let newItem = prompt("What item do you wanna add?", "N/A");
-		let newAmount  = prompt("How many of this item do you have?", 1)
-		setName(newItem);
-		setAmount(newAmount);
-	}
+    function handleClickAdd() {
+        let newItem = prompt("What item do you wanna add?", "N/A");
+        let newAmount = prompt("How many of this item do you have?", 1);
+        setName(newItem);
+        setAmount(newAmount);
+    }
 
-	const textStyle = {
-		width: '60%',
-		display: 'flex',
-		flexDirection: "row",
-    	justifyContent: "space-between",
-	};
+    const textStyle = {
+        width: "60%",
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-between",
+    };
 
-	const button = _amount > 0 ? <button onClick={handleClickUse}>use</button> : <button onClick={handleClickAdd}>add</button>;
+    const button =
+        _amount > 0 ? <button onClick={handleClickUse}>use</button> : <button onClick={handleClickAdd}>add</button>;
 
-	const amountText = _amount > 1 ? `${_amount}x ` : '';
-	const content = (
-		<p style={textStyle}>
-			{amountText}
-			{_name}
-			{button}
-		</p>
-	);
+    const amountText = _amount > 1 ? `${_amount}x ` : "";
+    const content = (
+        <p style={textStyle}>
+            {amountText}
+            {_name}
+            {button}
+        </p>
+    );
 
-	return content;
+    return content;
 }
 
 export default Gear;
